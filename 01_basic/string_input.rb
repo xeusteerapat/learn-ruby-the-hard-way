bfc4622c49997635fb12f8111eae6ea41dc17e58
xeusteerapat <xeus085@gmail.com>
2020-12-02 22:54:06 +0700

    [add] input string

---
+++ 01_basic/string_input.rb
@@ -0,0 +1,9 @@
+# using keyword "gets.chomp" for input
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} years old, #{height} cm. and #{weight} kg."
