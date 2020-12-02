f0575b48f8700fcc938dceb516b2033889c2a631
xeusteerapat <xeus085@gmail.com>
2020-12-02 13:31:55 +0700

    [add] more string and text

---
+++ 01_basic/string_and_text.rb
@@ -0,0 +1,19 @@
+types_of_people = 10
+
+# string interpolation
+x = "There are #{types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+# string concatenation
+w = "This is the left side of..."
+e = "a string with a right side."
+
+puts w + e
+
+test = "test string"
+puts 'no test show #{test}' # single quote ignore any variables inside it
