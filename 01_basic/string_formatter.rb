65189ebdcb44704a737a4d5f4212f7c2f123f766
xeusteerapat <xeus085@gmail.com>
2020-12-02 13:42:03 +0700

    [edit] new line printing string

--- 01_basic/string_formatter.rb
+++ 01_basic/string_formatter.rb
@@ -20,3 +20,18 @@ puts formatter % {
   %{first} %{second} %{third} %{forth} %{first} %{second} %{third} %{forth} %{first} %{second} %{third} %{forth} %{first} %{second} %{third} %{forth}
   I had this thing That you could type up right But it didn't sing So I said goodnight  
 =end
+
+# new line string \n or using %q
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts %q{
+There's something going on here.
+With this weird quote
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+}
