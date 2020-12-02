c3412cc4a434b2bc3f7c7126ac8526036107a0fe
xeusteerapat <xeus085@gmail.com>
2020-12-02 23:03:22 +0700

    [add] unpacking variables

---
+++ 01_basic/unpack_variables.rb
@@ -0,0 +1,14 @@
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
+# how to run
+# ruby 01_basic/unpack_variables.rb first second third
+# then you will see
+"" "
+Your first variable is: first
+Your second variable is: second
+Your third variable is: third
+" ""
