b54105244acd9f42fa2b728f874ba8cb7a899ef8
xeusteerapat <xeus085@gmail.com>
2020-12-02 22:50:09 +0700

    [add] escape characters

---
+++ 01_basic/escape_char.rb
@@ -0,0 +1,22 @@
+tabby_cat = "\tI'm tabbed in"
+persian_cat = "I'm split\non a line"
+backslash_cat = "I'm \\ a \\ cat."
+
+puts backslash_cat
+
+fat_cat = "" "
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+" ""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+
+=begin
+  \t tab
+  \n new line
+=end
