50345e62f468486b7bf56cee748104dfe7e33f76
xeusteerapat <xeus085@gmail.com>
2020-12-02 22:56:42 +0700

    [add] more error case

--- 01_basic/string_input.rb
+++ 01_basic/string_input.rb
@@ -7,3 +7,8 @@ print "How much do you weigh? "
 weight = gets.chomp
 
 puts "So, you're #{age} years old, #{height} cm. and #{weight} kg."
+
+# error, gets.chomp always gets string
+print "Your fav num?"
+fav_num = gets.chomp
+puts "Your fav num plus 5 is #{fav_num + 5}"
